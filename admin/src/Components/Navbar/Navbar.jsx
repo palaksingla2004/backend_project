30a3ac94014ff4bd3b14d441f6b5ec0d9a47f119
palaksingla2004 <palaksingla1401@gmail.com>
2024-11-25 19:54:07 +0530

    admin1

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className='navbar'>
       
       <img src={navlogo} className='nav-logo' alt="" />
-      <div className='alo'>
+      <div className='alo' style={{ textAlign: "left", flex: 1 }}>
       <h3 align="left">ALO</h3>
       </div>
       <img src={navprofileIcon} className='nav-profile' alt="" />
